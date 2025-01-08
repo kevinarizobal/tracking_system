5c78b23b9d368fb53e29919e6b9a668aa34ce3d6
FDC.KEVIN <fdc.kevinar@gmail.com>
2025-01-08 08:16:18 +0800

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include('link/header.php'); ?>
+</head>
+<body>
+<?php include('link/navbar.php');?>
+<?php include('link/script.php');?>
+</body>
+</html>
