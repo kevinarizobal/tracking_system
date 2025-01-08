184c81cdd0627d57f2536bec6ccd1255c9f0c0bb
FDC.KEVIN <fdc.kevinar@gmail.com>
2025-01-08 08:43:16 +0800

    Update input field whether email or username and add footer below login button

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <title>NEMSU-SUPTRACK</title>
-</head>
+<?php include("link/header.php");?>
 <body>
     <!----------------------- Main Container -------------------------->
      <div class="container d-flex justify-content-center align-items-center min-vh-100">
@@ -16,7 +9,7 @@
     <!--------------------------- Left Box ----------------------------->
        <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box" style="background: #103cbe;">
            <div class="featured-image mb-3">
-            <img src="images/1.png" class="img-fluid" style="width: 250px;">
+            <img src="image/2.png" class="img-fluid mt-2" style="width: 250px;">
            </div>
            <p class="text-white fs-2" style="font-family: 'Courier New', Courier, monospace; font-weight: 600;">SUPTRACK</p>
            <small class="text-white text-wrap text-center" style="width: 17rem;font-family: 'Courier New', Courier, monospace;">Design By @OnaisYaj</small>
@@ -26,24 +19,28 @@
        <div class="col-md-6 right-box">
           <div class="row align-items-center">
                 <div class="header-text mb-4">
+                    <h2><i class="bi bi-person-circle"></i>&nbsp;Login</h2>
                 </div>
                 <div class="input-group mb-3">
-                    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Email address">
+                    <span class="input-group-text bg-light"><i class="bi bi-person"></i></span>
+                    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Email address or Username">
                 </div>
-                <div class="input-group mb-1">
+                <div class="input-group mb-3">
+                    <span class="input-group-text bg-light"><i class="bi bi-lock"></i></span>
                     <input type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password">
                 </div>
                 <div class="input-group mb-5 d-flex justify-content-between">
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="formCheck">
-                        <label for="formCheck" class="form-check-label text-secondary"><small>Remember Me</small></label>
-                    </div>
                     <div class="forgot">
                         <small><a href="#">Forgot Password?</a></small>
                     </div>
                 </div>
                 <div class="input-group mb-3">
                     <button class="btn btn-lg btn-primary w-100 fs-6">Login</button>
+                </div>
+                <div class="input-group mb-3 justify-content-center text-secondary">
+                    <div class="forgot">
+                        <small><i>&copy;&nbsp;2024 NEMSU Cantilan All Right Reserved</i></small>
+                    </div>
                 </div>
           </div>
        </div> 
