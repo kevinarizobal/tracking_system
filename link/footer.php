e369a9c1cbe7d61e1b119132e02d46cddde2cb8e
FDC.KEVIN <fdc.kevinar@gmail.com>
2025-01-29 17:58:17 +0800

    Add files via upload

---
+++ link/footer.php
@@ -0,0 +1,3 @@
+<footer class="bg-white shadow d-flex justify-content-center align-items-center text-primary" style="height: 60px;">
+    <p class="mb-0 align-items-center">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&copy; 2024 NEMSU Cantilan. All Rights Reserved. Developed By @OnaisYaj</p>
+</footer>
