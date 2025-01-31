63ce5b58f647c454ffb15aa8fff7aea27555dbf0
FDC.KEVIN <fdc.kevinar@gmail.com>
2025-01-31 10:19:08 +0800

    Update fetch_related_items.php

--- fetch_related_items.php
+++ fetch_related_items.php
@@ -3,7 +3,7 @@ include('config.php');
 
 if (isset($_GET['property_number'])) {
     $property_number = $_GET['property_number'];
-    $sql = "SELECT id, qty, unit, description, property_number, date_acquired, amount, SUM(amount) as totalamount FROM par_tb WHERE property_number = ?";
+    $sql = "SELECT * FROM par_tb WHERE property_number = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param('s', $property_number);
     $stmt->execute();
