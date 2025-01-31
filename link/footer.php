39cfbe29302a9de1e48ce829091108e27621b1c1
FDC.KEVIN <fdc.kevinar@gmail.com>
2025-01-31 11:41:23 +0800

    Update footer.php

--- link/footer.php
+++ link/footer.php
@@ -1,3 +1,41 @@
-<footer class="bg-white shadow d-flex justify-content-center align-items-center text-primary" style="height: 60px;">
-    <p class="mb-0 align-items-center">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&copy; 2024 NEMSU Cantilan. All Rights Reserved. Developed By @OnaisYaj</p>
-</footer>
+<!-- Footer -->
+<div class="container-fluid bg-primary text-white footer mt-5" data-wow-delay="0.1s">
+    <div class="container py-4">
+        <div class="row g-5">
+            <!-- Information Center -->
+            <div class="col-lg-6 col-md-6">
+                <h5 class="text-white mb-4">Information Center</h5>
+                <p class="mb-2"><i class="bi bi-geo-alt me-3"></i><strong>Address:</strong> Cantilan, 8317 Surigao del Sur</p>
+                <p class="mb-2"><i class="bi bi-telephone me-3"></i><strong>Phone:</strong> 0918 427 7560</p>
+                <p class="mb-2"><i class="bi bi-envelope me-3"></i><strong>Email:</strong> information@nemsu.edu.ph</p>
+                <p class="mb-2"><i class="bi bi-clock me-3"></i><strong>Office Hours:</strong> Mon - Fri, 8:00 AM - 5:00 PM</p>
+                <p class="mb-2"><i class="bi bi-globe me-3"></i><strong>Website:</strong> <a href="https://www.nemsu.edu.ph" class="text-white">www.nemsu.edu.ph</a></p>
+            </div>
+
+            <!-- Photo Gallery -->
+            <div class="col-lg-6 col-md-6">
+                <h5 class="text-white mb-4">Photo Gallery</h5>
+                <div class="row g-2 pt-2">
+                    <div class="col-4">
+                        <img class="img-fluid rounded bg-light p-1 w-100 d-block" src="image/test.png" alt="Gallery Image 1" style="height: 100px; object-fit: cover;">
+                    </div>
+                    <div class="col-4">
+                        <img class="img-fluid rounded bg-light p-1 w-100 d-block" src="image/test2.jpg" alt="Gallery Image 2" style="height: 100px; object-fit: cover;">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Copyright Section -->
+    <div class="container">
+        <div class="copyright">
+            <div class="row">
+                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                    &copy; 2024 NEMSU Cantilan. All Rights Reserved. Developed By @OnaisYaj
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
